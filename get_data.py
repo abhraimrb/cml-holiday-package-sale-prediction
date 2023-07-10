a67db94e75fc2c72452769f2342020ab91c6235a
abhraimrb <30985012+abhraimrb@users.noreply.github.com>
2023-07-10 10:59:57 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -12,41 +12,7 @@ package_sale_data = pd.read_csv('data/holiday_package_data.csv')
 package_sale_data = package_sale_data.drop(columns = ['CustomerID'])
 package_sale_data.replace('Fe Male','Female', inplace = True)
 
-numeric_features = [
-        'Age', 
-        'DurationOfPitch', 
-        'MonthlyIncome'
-    ]
 
-    numeric_transformer = Pipeline(
-        steps = [
-            ('imputer', SimpleImputer(strategy = 'median')), 
-            ("scaler", StandardScaler())
-        ]
-    )
-
-    categorical_features = [
-        'TypeofContact', 
-        'Occupation', 
-        'Gender', 
-        'ProductPitched',
-        'MaritalStatus',
-        'Designation'
-    ]
-
-    categorical_transformer = Pipeline(
-        steps = [
-            ('imputer', SimpleImputer(strategy = 'most_frequent')),
-            ('encoder', OneHotEncoder(handle_unknown = 'ignore'))
-        ]
-    )
-    
-    preprocessor = ColumnTransformer(
-        transformers = [
-            ('num_tr', numeric_transformer, numeric_features),
-            ('cat_tr', categorical_transformer, categorical_features)
-        ], remainder = SimpleImputer(strategy = 'most_frequent')
-    )
 X = package_sale_data.drop(columns = ['ProdTaken'])
 y = package_sale_data['ProdTaken']# Make a train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
