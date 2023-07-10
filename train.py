2eef49a78fd034b01e9137106092fa612e41e4c6
abhraimrb <30985012+abhraimrb@users.noreply.github.com>
2023-07-10 10:14:56 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,11 @@
+import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,9 +19,12 @@ y_test = np.genfromtxt("data/test_labels.csv")
 # Fit a model
 depth = 2
 clf = RandomForestClassifier(max_depth=depth)
-clf.fit(X_train, y_train)
+pipe = Pipeline(steps = [
+            ('preprocessor', preprocessor), ('classifier', model)
+        ])
+pipe_rf.fit(X_train, y_train)
 
-acc = clf.score(X_test, y_test)
+acc = pipe_rf.score(X_test, y_test)
 print(acc)
 with open("metrics.txt", "w") as outfile:
     outfile.write("Accuracy: " + str(acc) + "\n")
