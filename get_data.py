8461e61e0fd1e802de0717ee8fd37569a71ceae8
abhraimrb <30985012+abhraimrb@users.noreply.github.com>
2023-07-10 11:20:16 +0530

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -18,7 +18,7 @@ y = package_sale_data['ProdTaken']# Make a train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 
 
-np.savetxt('data/train_features.csv', X_train)
-np.savetxt('data/test_features.csv', X_test)
-np.savetxt('data/train_labels.csv', y_train)
-np.savetxt('data/test_labels.csv', y_test)
+X_train.to_csv('data/train_features.csv', index=False)
+X_test.to_csv('data/test_features.csv', index=False)
+y_train.to_csv('data/train_labels.csv', index=False)
+y_test.to_csv('data/test_labels.csv', index=False)
